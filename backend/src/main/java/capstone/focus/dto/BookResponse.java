2b457298cdb0049e0acfc0064f659690ba973d26
Soomin-Lim <augustth511@gmail.com>
2023-04-16 14:30:13 +0900

    feat: Response DTO 객체의 @Setter 제거 후 final 키워드를 붙여 불변 객체로 수정
    Response 객체는 생성자로 객체가 생성될 때 데이터가 고정되므로, 불변 객체로 만들어 데이터를 전달하는 과정에서 데이터가 변조되지 않음을 보장한다.

--- backend/src/main/java/capstone/focus/dto/BookResponse.java
+++ backend/src/main/java/capstone/focus/dto/BookResponse.java
@@ -2,15 +2,13 @@ package capstone.focus.dto;
 
 import capstone.focus.domain.Book;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 public class BookResponse {
-    private Long bookId;
-    private String title;
-    private String author;
-    private String description;
+    private final Long bookId;
+    private final String title;
+    private final String author;
+    private final String description;
 
     public BookResponse(Book book) {
         this.bookId = book.getId();
--- backend/src/main/java/capstone/focus/dto/SignUpResponse.java
+++ backend/src/main/java/capstone/focus/dto/SignUpResponse.java
@@ -1,7 +1,6 @@
 package capstone.focus.dto;
 
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
 public class SignUpResponse {
