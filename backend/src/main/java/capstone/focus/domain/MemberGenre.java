b1443ace1347e204cfbb3e862b501d43dda0b251
Soomin-Lim <augustth511@gmail.com>
2023-05-09 15:39:51 +0900

    feat: MemberGenre 엔티티 클래스 생성
    member 테이블과 genre 테이블이 다대다 관계이므로 이 두 테이블을 연결하는 연결 테이블이 필요하다. 따라서 이 연결 테이블을 나타내기 위해 MemberGenre 엔티티 클래스를 생성했다.

---
+++ backend/src/main/java/capstone/focus/domain/MemberGenre.java
@@ -0,0 +1,30 @@
+package capstone.focus.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "member_genre")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberGenre {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_genre_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "genre_id")
+    private Genre genre;
+
+    @Column(name = "is_deleted", columnDefinition = "boolean default false")
+    private boolean isDeleted;
+}
