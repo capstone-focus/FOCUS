7fa82c2254ac44e9476c48f94b7dbe46fb190f96
Soomin Lim <63943319+Soomin-Lim@users.noreply.github.com>
2023-05-16 21:44:25 +0900

    feat: 세션을 사용한 로그인 및 로그아웃 기능 구현 (#9)
    * feat: 사용자가 로그인 요청 시 access token과 refresh token 값을 전달 받지 않도록 변경
    
    access token, refresh token은 next.js에서 쿠키로 저장 및 사용이 가능하므로, DB에 따로 저장할 필요가 없다. 따라서 LoginRequest DTO와 Member 엔티티에서 accessToken, refreshToken 필드를 삭제하였다.
    
    * feat: 세션을 사용한 로그인 기능 구현 및 회원이 최초 로그인한 경우 응답 메시지 변경
    
    세션을 사용하여 로그인 기능을 구현하였다. 또한 로그인 API 에서 최초 로그인한 경우 응답 메시지에서 기존에는 memberId를 포함하였는데, memberId 값을 프론트에 전달할 필요 없이 세션에 저장하면 되므로, 로그인 API 응답 메시지 스펙을 두 경우 모두 통일했다.
    
    * feat: 세션을 사용하여 로그아웃 기능 구현
    
    * feat: 로그아웃이 잘 동작되는지 확인하기 위한 로그 추가

--- backend/src/main/java/capstone/focus/domain/Member.java
+++ backend/src/main/java/capstone/focus/domain/Member.java
@@ -4,10 +4,8 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "member")
@@ -15,7 +13,8 @@ import java.time.LocalDateTime;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "member_id")
     private Long id;
 
@@ -23,29 +22,13 @@ public class Member {
 
     private String email;
 
-    @Column(name = "access_token")
-    private String accessToken;
-
-    @Column(name = "refresh_token")
-    private String refreshToken;
-
-    @Column(name = "expired_at")
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime expiredAt;
-
     @Builder
-    public Member(String name, String email, String accessToken, String refreshToken, LocalDateTime expiredAt) {
+    public Member(String name, String email) {
         this.name = name;
         this.email = email;
-        this.accessToken = accessToken;
-        this.refreshToken = refreshToken;
-        this.expiredAt = expiredAt;
     }
 
-    public void update(String name, String accessToken, String refreshToken) {
+    public void update(String name) {
         this.name = name;
-        this.accessToken = accessToken;
-        this.refreshToken = refreshToken;
-        this.expiredAt = LocalDateTime.now().plusHours(1);
     }
 }
