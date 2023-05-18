c8722f71b795f883c19ad759ad34c69581aff0a2
Soomin-Lim <augustth511@gmail.com>
2023-05-18 14:20:15 +0900

    feat: ChatGPT에게 받을 응답값의 JSON의 태그 중 'reason' 태그 내용을 100자 이하로 제한
    reason 태그 내용의 길이에 제한을 두지 않으면, 이후 ChatGPT로부터 응답을 받을 때 응답의 길이가 너무 길어 내용이 도중에 짤릴 수 있다. 즉, 완전한 JSON 형태로 전달받지 못해 Jackson 라이브러리로 객체 변환 시 Exception이 발생할 수 있다. 따라서 'reason' 태그 내용을 100자 이하로 제한하였다.

--- backend/src/main/java/capstone/focus/domain/ChatGptRequestConst.java
+++ backend/src/main/java/capstone/focus/domain/ChatGptRequestConst.java
@@ -12,6 +12,7 @@ public abstract class ChatGptRequestConst {
             "'artist' tag show the name of songwriter. 'spotifyId' tag show the Spotify id of the song.\n" +
             "'recommendedSongs' tag includes every information about your recommended songs.\n" +
             "'reason' tag explains why chatGPT recommend this song for this book based on contents & sentiment of book.\n" +
+            "We will limit the content of the 'reason' tag to 100 characters or less.\n" +
             "NEVER response any other explanation except JSON Format. Response JUST JSON Format.";
 
     public static final String assistantMessage = "Thank you for letting me know. I will recommend music that would\n" +
