670e9c6acfc8e240cf294b4b67455ec282e398d0
Soomin-Lim <augustth511@gmail.com>
2023-05-19 17:53:03 +0900

    docs: 테이블을 생성 DDL 수정
    - 책의 제목은 유일하지 않을 수 있으므로 book 테이블의 title 속성의 unique 제약조건 제거
    - chapter 테이블의 description 속성은 null 값이 될 수 있기 때문에 not null 제약조건 제거

--- backend/sql/ddl.sql
+++ backend/sql/ddl.sql
@@ -3,14 +3,14 @@ create table book (
         author varchar(255) not null,
         description text not null,
         image_url varchar(255),
-        title varchar(255) not null unique,
+        title varchar(255) not null,
         primary key (book_id)
     ) engine=InnoDB;
 
     create table chapter (
        book_id bigint not null,
         chapter_seq integer not null,
-        description text not null,
+        description text,
         primary key (book_id, chapter_seq),
         foreign key(book_id) references book(book_id)
     ) engine=InnoDB;
