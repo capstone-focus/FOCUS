86676b3d5b99c51c5f33aaf18ac324f97debcfdd
Soomin-Lim <augustth511@gmail.com>
2023-05-19 16:36:33 +0900

    feat: BookController 클래스에 책 이미지 조회 API 요청을 처리하는 로직 구현
    - bookImage(): 경로변수로 주어진 bookId를 통해 해당 책의 이미지를 조회하여 응답값으로 보내는 로직 구현
    - 책 이미지가 존재하면 Content-Type을 image/jpeg로 설정하여 응답 메시지를 전송
    - 책 이미지가 존재하지 않으면 404 Not Found 응답 메시지를 전송

--- backend/src/main/java/capstone/focus/controller/BookController.java
+++ backend/src/main/java/capstone/focus/controller/BookController.java
@@ -1,7 +1,11 @@
 package capstone.focus.controller;
 
 import capstone.focus.service.BookService;
+import capstone.focus.service.LocalFileService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,9 +14,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class BookController {
 
     private final BookService bookService;
+    private final LocalFileService localFileService;
 
     @GetMapping("/books")
     public ResponseEntity<?> bookList(@RequestParam(defaultValue = "0") int page) {
@@ -22,5 +28,20 @@ public class BookController {
     @GetMapping("/books/{bookId}")
     public ResponseEntity<?> bookDetail(@PathVariable Long bookId) {
         return ResponseEntity.ok(bookService.bookDetail(bookId));
+    }
+
+    @GetMapping("/books/{bookId}/image")
+    public ResponseEntity<?> bookImage(@PathVariable Long bookId) {
+        String bookTitle = bookService.getBookTitle(bookId);
+        UrlResource bookImage = localFileService.getBookImage(bookTitle);
+
+        if (bookImage != null) {
+            return ResponseEntity.ok().
+                    contentType(MediaType.IMAGE_JPEG)
+                    .body(bookImage);
+        }
+
+        log.info("Not Found");
+        return ResponseEntity.notFound().build();
     }
 }
