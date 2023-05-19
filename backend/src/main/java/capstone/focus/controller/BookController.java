226e1bf75a7ce1f283e399ad03a41241cff1fb5e
Soomin-Lim <augustth511@gmail.com>
2023-05-19 16:58:38 +0900

    refactor: @Controller 어노테이션을 @RestController 어노테이션으로 변경
    @RestController는 @Controller와 달리 내부적으로 @ResponseBody 어노테이션을 포함하고 있어 모든 핸들러 메서드의 반환 값이 응답의 본문으로 직접 전송되도록 한다.
    BookController, MemberController 모두 RestAPI 요청을 처리하기 때문에 @RestController 메서드로 변경하였다.

--- backend/src/main/java/capstone/focus/controller/BookController.java
+++ backend/src/main/java/capstone/focus/controller/BookController.java
@@ -7,12 +7,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @Slf4j
 public class BookController {
